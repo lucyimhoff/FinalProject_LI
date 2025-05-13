4313f2ef51cb2124ea06997dc7425605e4acbad9
lucyimhoff <lucyi4@nycstudents.net>
2025-05-13 13:30:48 +0000

    updated

--- Main.java
+++ Main.java
@@ -7,19 +7,19 @@ public class Main{
     public static void main(String[] args){
         TrashSort test1 = new TrashSort();
         System.out.println(test1.sort());
-        JFrame frame = new JFrame("Background.png");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        Canvas canvas = new Drawing();
-        canvas.setSize(400,400);
-        frame.add(canvas);
-        frame.pack();
-        frame.setVisible(true);
+        //JFrame frame = new JFrame("Background.png");
+        //frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //Canvas canvas = new Drawing();
+        //canvas.setSize(400,400);
+        //frame.add(canvas);
+       // frame.pack();
+        //frame.setVisible(true);
 
 
 
     }
 
-    public void paint(Graphics g){
-        g.fillOval(100,100,200,200);
-    }
+   // public void paint(Graphics g){
+      //  g.fillOval(100,100,200,200);
+   // }
 }
