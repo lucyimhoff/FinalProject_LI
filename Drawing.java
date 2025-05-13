cacfc9218a612dc1ad8aed80eb2517281a1e8627
lucyimhoff <lucyi4@nycstudents.net>
2025-05-13 13:25:42 +0000

    trying to add graphics using canvas class

---
+++ Drawing.java
@@ -0,0 +1,8 @@
+import java.awt.Canvas;
+import java.awt.Graphics;
+import javax.swing.JFrame;
+// canvas class from books.trinket.io
+
+public class Drawing extends Canvas {
+
+}
--- Main.java
+++ Main.java
@@ -1,8 +1,25 @@
-import javax.imageio.ImageIO;
+import java.awt.Canvas;
+import java.awt.Graphics;
+import javax.swing.JFrame;
+
+
 public class Main{
-    public static void main(String arg[]){
+    public static void main(String[] args){
         TrashSort test1 = new TrashSort();
         System.out.println(test1.sort());
-        Image img = ImageIO.read(new File("Background.png"));
+        JFrame frame = new JFrame("Background.png");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Canvas canvas = new Drawing();
+        canvas.setSize(400,400);
+        frame.add(canvas);
+        frame.pack();
+        frame.setVisible(true);
+
+
+
+    }
+
+    public void paint(Graphics g){
+        g.fillOval(100,100,200,200);
     }
 }
